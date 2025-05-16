20aed17d246f87a122e7b64d5fc61561838975b1
Shreya090909 <2205103110011@paruluniversity.ac.in>
2025-05-16 11:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+  event.preventDefault(); 
+  const form = document.getElementById('nameForm');
+  const firstName = form.fname.value.trim();
+  const lastName = form.lname.value.trim();
 
+  const fullName = `${firstName} ${lastName}`.trim();
+
+  alert(fullName);
 }
